f3493595b5654d2a33dc2a4baf46f79da219f74f
JanarthanJana <janarthanr.pineappleai@gmail.com>
2025-02-10 15:20:34 +0530

    React Native training

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,13 +1,34 @@
-import { View,Text,Image,ImageBackground,ScrollView,Button,Pressable,Modal } from "react-native";
+import { View,Text,Image,ImageBackground,ScrollView,Button,Pressable,Modal,StatusBar,ActivityIndicator,Alert,StyleSheet } from "react-native";
 import { useState } from "react";
 import { getBackgroundColor } from './node_modules/react-native/Libraries/LogBox/UI/LogBoxStyle';
+import Greet from "./components/Greet";
+import Box from "./components/Box";
 const img = require('./assets/tv.jpg')
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false)
   return (
-    <View style={{ flex:1, backgroundColor: "plum", padding:60,}}>
-      <ScrollView>
+    <ScrollView>
+    <View style={ styles.container}>
+              {/* <StatusBar backgroundColor="lightgreen" barStyle="light-content" ></StatusBar>
+              <ActivityIndicator/>
+              <ActivityIndicator size="large" color="#0000ff" />
+              <ActivityIndicator size="large" color="#0000ff" animating={false}/> */}
+              <Button title="Alert" onPress={() => Alert.alert("Invalid Data!")} />
+              <Button title="Alert2" onPress={() => Alert.alert("Invalid Data!","DOB Incorrect!")} />
+              <Button title="Alert3" onPress={() => Alert.alert("Invalid Data!","DOB Incorrect!",[
+                {text: "Cancel", onPress: () => console.log("Cancel Pressed")},
+                {text: "OK", onPress: () => console.log("OK Pressed")}
+              ])} />
+
+              <Greet name="JANA"/>
+              <Greet name="Peter Parker"/>
+
+
+
+
+
+      {/* <ScrollView>
       <View style={{ height:200, width:200, backgroundColor: "blue"}}>
       </View>
       <View style={{ height:200, width:200, backgroundColor: "green"}}>
@@ -38,7 +59,99 @@ export default function App() {
           <Button title="close" color="midnightblue" onPress={()=>setIsModalVisible(false)}/>
           </View>
           </Modal>
-        </ScrollView>
+        </ScrollView> */}
+
+        <View style={[styles.lightbluebg,styles.box,styles.boxShadow]}>
+          <Text style={{borderRadius:5,backgroundColor:"red"}}>
+            Lightblue box
+          </Text>
+        </View>
+
+        <View style={[styles.lightgreenbg,styles.box,styles.androidShadow]}>
+          <Text>
+            Lightgreen box
+          </Text>
+        </View>
+
+        <View style={styles.darkMode}>
+          <Text style={styles.darkModeText}>
+            Style inheritance
+            <Text style={styles.boldText}>
+            in Bold
+          </Text>
+          </Text>
+        </View>
+
+        <View style={styles.boxView}>
+
+        <Box style={{backgroundColor:"lightblue"}}>BOX1</Box>
+        <Box style={{backgroundColor:"lightgreen"}}>BOX2</Box>
+
+        <Box style={{backgroundColor:"greenyellow"}}>BOX3</Box>
+
+        <Box style={{backgroundColor:"red"}}>BOX4</Box>
+
+        <Box style={{backgroundColor:"green"}}>BOX5</Box>
+        <Box style={{backgroundColor:"blue"}}>BOX6</Box>
+        <Box style={{backgroundColor:"pink"}}>BOX7</Box>
+
+        </View>
+
     </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: "plum",
+    padding:60,
+    marginTop:64,
+  },
+  boxView:{
+    marginTop:64,
+    borderWidth:6,
+    borderColor:"midnightblue"
+  },
+  darkMode:{
+    backgroundColor:"black",
+    color:"white"
+  },
+  darkModeText:{
+    color:"white"
+  },
+  boldText:{
+    fontWeight:"bold"
+  },
+  box:{
+    width:250,
+    height:250,
+    paddingHorizontal:10,
+    paddingVertical:20,
+    marginVertical:10,
+    borderWidth:2,
+    borderColor:"black",
+    borderRadius:10
+  },
+  lightbluebg:{
+    backgroundColor:"lightblue",
+    
+  },
+  lightgreenbg:{
+    backgroundColor:"lightgreen",
+    
+  },
+  boxShadow:{
+    shadowColor:"#333333",
+    shadowOffset: {
+      width:6,
+      height:6
+    },
+    shadowOpacity:0.6,
+    shadowRadius:4,
+  },
+  androidShadow:{
+    elevation:10
+  }
+})
